f1f208b38bdcc954df2bb3ec4d4da9f4ebea7a7a
Hack Reactor Students <dchk90@gmail.com>
2014-05-06 16:41:04 -0700

    Remove some comments

--- server/request-handler.js
+++ server/request-handler.js
@@ -30,27 +30,14 @@ exports.handleRequest = function(request, response) {
     }
   });
 
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
   var headers = defaultCorsHeaders;
 
   headers['Content-Type'] = "text/plain";
 
-  /* .writeHead() tells our server what HTTP status code to send back */
   response.writeHead(route(request), headers);
 
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.end(JSON.stringify({results: messages}));
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
