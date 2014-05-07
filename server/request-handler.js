2e9b1de062617b584bf6e761fd457926a232421f
Hack Reactor Students <dchk90@gmail.com>
2014-05-06 17:13:01 -0700

    Add handling different request methods

--- server/request-handler.js
+++ server/request-handler.js
@@ -6,21 +6,25 @@
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
 var handleRequest = function(request, response) {
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as what URL the browser is requesting. */
-
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
   console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode
+  if(request.method === 'GET') {
+    statusCode = 200;
+  } else if(request.method === 'POST') {
+    statusCode = 201;
+  } else if(request.method === 'OPTIONS') {
 
-  var statusCode = 200;
+  } else {
+    statusCode = 404;
+  }
+
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
   var headers = defaultCorsHeaders;
 
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   /* .writeHead() tells our server what HTTP status code to send back */
   response.writeHead(statusCode, headers);
@@ -29,7 +33,7 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  response.end(JSON.stringify({results: []}));
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
